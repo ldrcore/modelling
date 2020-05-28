066305d7f3f0c8d0d34f16ba8ba32a66019b0024
leonardo <leonardodarosa23@gmail.com>
2020-05-28 09:04:58 -0300

    Persistable trait to use into controllers, services or whatever class you want to persist models.

---
+++ src/Traits/Persistable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LDRCore\Modelling\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Trait Persistable
+ *
+ * @package LDRCore\Modelling\Traits
+ * @property Model $model
+ */
+class Persistable
+{
+    /**
+     * @param array $data
+     * @return Model
+     */
+    public function insert($data = []) : Model
+    {
+        return $this->model::create($data);
+    }
+    /**
+     * @param array $data
+     * @return Model
+     */
+    public function updateOrInsert($data = []) : Model
+    {
+        return $this->model::updateOrCreate($data);
+    }
+    /**
+     * @param int|array $ids
+     * @param array $data
+     * @return int
+     */
+    public function update($ids, $data = []) : int
+    {
+        return $this->model::whereIn('id', collect($ids))->update($data);
+    }
+    /**
+     * @param int|array $ids
+     * @return int
+     */
+    public function delete($ids) : int
+    {
+        return $this->model::whereIn('id', collect($ids))->delete();
+    }
+}
